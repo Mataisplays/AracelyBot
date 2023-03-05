38ef9534e90a797cfc194c3642054956ee809d11
Mataisplays <mateo20102020@gmail.com>
2023-03-05 11:23:57 -0600

    Format document

--- src/handlers/register.slashes.js
+++ src/handlers/register.slashes.js
@@ -3,7 +3,9 @@ const fs = require('node:fs');
 
 module.exports = function (client) {
 	const commands = [];
+
 	const commandFolders = fs.readdirSync('./src/commands');
+
 
 	for (const folder of commandFolders) {
 		const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
@@ -12,16 +14,20 @@ module.exports = function (client) {
 			commands.push(command.data.toJSON());
 		}
 	}
+
 	const rest = new REST({ version: '10' }).setToken(`${client.config.token}`);
 
 
 	(async () => {
 		try {
 			console.log(`Started refreshing ${commands.length} application (/) commands.`.yellow);
+
+
 			const data = await rest.put(
 				Routes.applicationGuildCommands(client.config.id, '841077479568441384'),
 				{ body: commands },
 			);
+
 			console.log(`Successfully refreshed ${data.length} application (/) commands.`.green);
 		} catch (error) {
 			console.error(error);
